7448639cabe8c79b230652a39ae6d881a698e6c9
AnnaClementson <35903649+AnnaClementson@users.noreply.github.com>
2020-01-03 15:47:52 +0000

    Add files via upload

---
+++ Arrays and Lists Exercise 1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friends = new List<string>();
+
+            Console.WriteLine("Enter friends names (hit enter twice to quit): ");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "")
+                    break;
+                friends.Add(input);
+            }
+
+            if (friends.Count == 1)
+            {
+                Console.WriteLine("{0} likes your post", friends[0]);
+                Console.ReadLine();
+            }
+            else if (friends.Count == 2)
+            {
+                Console.WriteLine("{0} and {1} like your post", friends[0], friends[1]);
+                Console.ReadLine();
+            }
+            else if (friends.Count == 3)
+            {
+                Console.WriteLine("{0}, {1} and {2} other person like your post", friends[0], friends[1], friends.Count - 2);
+                Console.ReadLine();
+            }
+            else if (friends.Count > 3)
+            {
+                Console.WriteLine("{0}, {1} and {2} other people like your post", friends[0], friends[1], friends.Count - 2);
+                Console.ReadLine();
+            } else
+                Console.WriteLine("Noone likes your post");
+                Console.ReadLine();
+        }
+    }
+}
